589b2abad81762449a09c0f84eb5054695c5b58a
Nellydhlamini <nellydhlamini34@gmail.com>
2023-04-20 04:35:29 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This program prints out a line using the puts function
+ * Return 0 -  This would mean the code was correct
+ */
+int main (void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+
+}
