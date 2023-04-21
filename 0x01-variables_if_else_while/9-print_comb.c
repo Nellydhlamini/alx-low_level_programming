462c4dcad69be159c8f73b7598750da9fc00186d
Nellydhlamini <nellydhlamini34@gmail.com>
2023-04-21 14:46:15 -0700

    Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main -  prints all possible combinations of single-digit numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
