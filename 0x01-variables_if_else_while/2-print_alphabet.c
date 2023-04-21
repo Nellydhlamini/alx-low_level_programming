ce3a044041de61ead150c31614620b005fa65d0a
Nellydhlamini <nellydhlamini34@gmail.com>
2023-04-21 10:48:03 -0700

    I sometimes suffer from insomnia.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Program prints the alphabet in lowercase
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int n = 97;
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+
+putchar('\n');
+
+
+return (0);
+}
