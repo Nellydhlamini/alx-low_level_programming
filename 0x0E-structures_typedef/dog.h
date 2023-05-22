6584c6139f9b1d1813c590834a23056230bbf551
Nellydhlamini <nellydhlamini34@gmail.com>
2023-05-22 05:54:18 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ * struct dog - dog attributes
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ * Description of the structure: Dog attributes
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
