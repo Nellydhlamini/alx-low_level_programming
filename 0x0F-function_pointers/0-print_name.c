0c5a4bd1895ef105f7e99c9cfcab45a22a9c8f05
Nellydhlamini <nellydhlamini34@gmail.com>
2023-05-24 08:55:11 -0700

    0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,6 +3,7 @@
  *  print_name - prints a name
  *  @name: name to be printed
  *  @f: pointer to function
+ *  Return: function that prints a name.
  */
 void print_name(char *name, void (*f)(char *))
 {
