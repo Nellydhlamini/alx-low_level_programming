d2c4881132b6290267d27fd8da80685bd35a1cc8
Nellydhlamini <nellydhlamini34@gmail.com>
2023-06-12 03:52:06 -0700

    1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _strlen - calculates the length of a string
+ * @s: the string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+int a = 0;
+
+while (s[a])
+a++;
+
+return (a);
+}
+
+/**
+ * create_file -  creates a file
+ * @filename: name of the file
+ * @text_content:  is a NULL terminated string to write to the file
+ * Return: 1 if successful, 0 if not
+ */
+int create_file(const char *filename, char *text_content)
+{
+int find;
+
+if (!filename)
+return (-1);
+
+find = open(filename, O_RDWR | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
+
+if (find == -1)
+return (-1);
+
+if (text_content)
+write(find, text_content, _strlen(text_content));
+
+close(find);
+return (1);
+}
