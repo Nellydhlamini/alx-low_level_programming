eede2c6632c8cbe6477432b67acb682a4e4c714c
Nellydhlamini <nellydhlamini34@gmail.com>
2023-06-12 03:32:30 -0700

    0. Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * read_textfile -  reads a text file and prints to a standard output.
+ * @filename: name of the file to be read
+ * @letters: number of letters to be read and printed
+ * Return: number of letters managed to be returned
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char *buff = malloc(sizeof(char *) * letters);
+int find, scan;
+
+if (!buff)
+return (0);
+
+if (!filename)
+return (0);
+
+find = open(filename, O_RDONLY, 0600);
+
+if (find == 1)
+return (0);
+
+scan = read(find, buff, letters);
+write(STDOUT_FILENO, buff, scan);
+
+free(buff);
+close(find);
+return (scan);
+}
